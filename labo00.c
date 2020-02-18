f68e28f079fb42d1aabe5bd7d34a1eea606ee9ed
Shannthujan <shannthujan.thuraisingham@heig-vd.com>
2020-02-18 21:58:19 +0100

    Update labo00.c

--- labo00.c
+++ labo00.c
@@ -1,1 +1,36 @@
 // labo 00
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main(void)
+{
+
+	char TAB[100];
+	int k = 0, m = 0, n = 1;
+	printf("Entrez une chaine de caractere\n");
+	for (; k < 100; k++) {
+		scanf("%c", &TAB[k]);
+		m++;
+		if (TAB[k] == ' ') {
+			n++;
+		}
+		else if (TAB[k] == '\n') {
+			k = 100;
+		}
+	}
+
+	printf("%d\n", n);
+
+	for (k = 0; k < m; k++) {
+		printf("%c", TAB[k]);
+		if (TAB[k] == ' ') {
+			printf("\n");
+		}
+	}
+
+	system("PAUSE");
+	return EXIT_SUCCESS;
+}
